27826408b52264fc4fff18885a67c44ea6631909
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-12-03 19:40:35 -0700

    Use radiate, add alt attribute, update title

--- .wick-temp/pack_source.js
+++ .wick-temp/pack_source.js
@@ -1,2 +1,2 @@
-export * as MkQNge4iw from "/home/work/projects/realm_front_end_mentor/nft-preview-card-component-main/node_modules/@candlelib/glow";
-export * as wick from "@candlelib/wick/source/typescript/entry/wick-runtime.ts"
+export * as MccdVAicX from "@candlelib/glow";
+export * as radiate from "@candlelib/wick/source/typescript/entry/wick-radiate.ts"
