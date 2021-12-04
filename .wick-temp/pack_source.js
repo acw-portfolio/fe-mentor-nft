6ad2d9924b5ced78d52bec30f525f0f2bec75c31
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-12-03 19:02:55 -0700

    Build wick component and compile to www

---
+++ .wick-temp/pack_source.js
@@ -0,0 +1,2 @@
+export * as MkQNge4iw from "/home/work/projects/realm_front_end_mentor/nft-preview-card-component-main/node_modules/@candlelib/glow";
+export * as wick from "@candlelib/wick/source/typescript/entry/wick-runtime.ts"
